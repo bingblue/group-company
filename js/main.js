1b8b2e8b131d01cfe9225483e9fac0676e8f1184
gavin <gavin.gao@e3-expo.com>
2017-12-28 15:14:34 +0800

    index

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 'use strict'
 $(function () {
-
+  $('.bg3 li').mouseenter(function(){
+    $('.bg3 .active').removeClass('active')
+    $(this).addClass('active')
+  })
+  $('.demo li').mouseenter(function() {
+    $('.demo .active').removeClass('active')
+    $(this).addClass('active')
+  })
 })
