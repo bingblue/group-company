a0c85b40e180faf412b46cef0e8277616c3fa922
XiaoMuCOOL <895355044@qq.com>
2018-01-18 14:16:51 +0800

    update

--- js/main.js
+++ js/main.js
@@ -102,18 +102,18 @@ function main() {
             }
         });
         $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
+            // $('.cat .active').removeClass('active');
+            // $(this).addClass('active');
+            // var selector = $(this).attr('data-filter');
+            // $container.isotope({
+            //     filter: selector,
+            //     animationOptions: {
+            //         duration: 750,
+            //         easing: 'linear',
+            //         queue: false
+            //     }
+            // });
+            // return false;
         });
 
     });
